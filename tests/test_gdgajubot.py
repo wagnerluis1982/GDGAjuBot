11b355caca0ca87cb8321a4e1f246f8e2af308b2
Erick Mendonça <erickmendonca@yahoo.com.br>
2016-03-28 14:41:39 -0300

    Corrige identação do arquivo de testes

--- tests/test_gdgajubot.py
+++ tests/test_gdgajubot.py
@@ -9,20 +9,20 @@ import os
 import gdgajubot
 
 class TestGDGAjuBot:
-	def test_find_ruby(self):
-		assert gdgajubot.find_ruby("Olá ruby GDG")
-		assert gdgajubot.find_ruby("Olá RUBY GDG")
-		assert gdgajubot.find_ruby("Olá Ruby GDG")
-		assert gdgajubot.find_ruby("OlárubyGDG")
+    def test_find_ruby(self):
+        assert gdgajubot.find_ruby("Olá ruby GDG")
+        assert gdgajubot.find_ruby("Olá RUBY GDG")
+        assert gdgajubot.find_ruby("Olá Ruby GDG")
+        assert gdgajubot.find_ruby("OlárubyGDG")
 
-	def test_find_java(self):
-		assert gdgajubot.find_java("Olá java GDG")
-		assert gdgajubot.find_java("Olá Java GDG")
-		assert gdgajubot.find_java("Olá JAVA GDG")
-		assert gdgajubot.find_java("OlájavaGDG")
+    def test_find_java(self):
+        assert gdgajubot.find_java("Olá java GDG")
+        assert gdgajubot.find_java("Olá Java GDG")
+        assert gdgajubot.find_java("Olá JAVA GDG")
+        assert gdgajubot.find_java("OlájavaGDG")
 
-	def test_find_python(self):
-		assert gdgajubot.find_python("Olá python GDG")
-		assert gdgajubot.find_python("Olá Python GDG")
-		assert gdgajubot.find_python("Olá PYTHON GDG")
-		assert gdgajubot.find_python("OlápythonGDG")
+    def test_find_python(self):
+        assert gdgajubot.find_python("Olá python GDG")
+        assert gdgajubot.find_python("Olá Python GDG")
+        assert gdgajubot.find_python("Olá PYTHON GDG")
+        assert gdgajubot.find_python("OlápythonGDG")
