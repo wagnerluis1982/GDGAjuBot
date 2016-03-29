b294d0990be9b17f9cfde50819f9ddf58fbe5ea6
Wagner Macedo <wagnerluis1982@gmail.com>
2016-03-29 11:59:37 -0300

    Adicionado test_list_upcoming_events

--- tests/test_gdgajubot.py
+++ tests/test_gdgajubot.py
@@ -35,12 +35,37 @@ class MockTeleBot:
 class MockMessage:
     # Método definido para não ter AttributeError
     def __getattribute__(self, item):
-        pass
+        return self
 
 
 class MockResources:
     def get_events(self):
-        pass
+        return [
+            {'event_url': 'http://www.meetup.com/GDG-Aracaju/events/229313880/',
+             'name': 'Hackeando sua Carreira #Hangout',
+             'time': 1459378800000},
+            {'event_url': 'http://www.meetup.com/GDG-Aracaju/events/229623381/',
+             'name': 'Android Jam 2: Talks Dia 2',
+             'time': 1459612800000},
+            {'event_url': 'http://www.meetup.com/GDG-Aracaju/events/mwnsrlyvgbjb/',
+             'name': 'Coding Dojo',
+             'time': 1459980000000},
+            {'event_url': 'http://www.meetup.com/GDG-Aracaju/events/229591464/',
+             'name': 'O Caminho para uma Arquitetura Elegante #Hangout',
+             'time': 1460160000000},
+            {'event_url': 'http://www.meetup.com/GDG-Aracaju/events/229770309/',
+             'name': 'Android Jam 2: #Curso Dia 2',
+             'time': 1460217600000},
+            {'event_url': 'http://www.meetup.com/GDG-Aracaju/events/mwnsrlyvhbgb/',
+             'name': 'Coding Dojo',
+             'time': 1462399200000},
+            {'event_url': 'http://www.meetup.com/GDG-Aracaju/events/229951204/',
+             'name': 'Google I/O Extended',
+             'time': 1463587200000},
+            {'event_url': 'http://www.meetup.com/GDG-Aracaju/events/229951264/',
+             'name': 'Google IO Extended 2016',
+             'time': 1463608800000},
+        ]
 
     def get_packt_free_book(self):
         pass
@@ -72,3 +97,15 @@ class TestGDGAjuBot(unittest.TestCase):
         g_bot.send_welcome(message)
         self.assertEqual(bot.calls[-1],
                          CALL.reply_to(message, "Este bot faz buscas no Meetup do Test-Bot"))
+
+    def test_list_upcoming_events(self):
+        bot, resources, message = MockTeleBot(), MockResources(), MockMessage()
+        g_bot = gdgajubot.GDGAjuBot(bot, resources, {})
+        g_bot.list_upcoming_events(message)
+        r = ("Hackeando sua Carreira #Hangout: 30/03 http://www.meetup.com/GDG-Aracaju/events/229313880/\n"
+             "Android Jam 2: Talks Dia 2: 02/04 http://www.meetup.com/GDG-Aracaju/events/229623381/\n"
+             "Coding Dojo: 06/04 http://www.meetup.com/GDG-Aracaju/events/mwnsrlyvgbjb/\n"
+             "O Caminho para uma Arquitetura Elegante #Hangout: 08/04 http://www.meetup.com/GDG-Aracaju/events/229591464/\n"
+             "Android Jam 2: #Curso Dia 2: 09/04 http://www.meetup.com/GDG-Aracaju/events/229770309/")
+        self.assertEqual(bot.calls[-1],
+                         CALL.reply_to(message, r, disable_web_page_preview=True))
